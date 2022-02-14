977d44be55bea92a210678f393134f10cdf01e73
Alex <proshanov@yandex.ru>
2022-02-14 22:32:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// let  number = 4.6;
+// console.log(-4/0);
+// console.log("string" * 9);
+
+// const person = "Alex";
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.isMarried);
+// console.log(obj["isMarried"]);
+
+// let arr = ["plum.png", "orange.jpg", "apple.bmp", 6, {}, []];
+// console.log(arr[0]);
+
+// alert("hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "0");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("Your name?", "");
+// answers[1] = prompt("Your last name?", "");
+// answers[2] = prompt("Your age?", "");
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https://url.com/${category}/5`)
+
+// const user = "alex";
+
+// console.log(`Hello, ${user}`);
+
+// Операторы в JS
+// ---------------------------------------------
+
+// console.log("arr" + "- object");
+// console.log(4 + " - object");
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 == "8");
+console.log(2*4 === "8");
+console.log(2 + 2 * 2 === "8");
+console.log(2 + 2 * 2 != "8");
+console.log(2 + 2 * 2 !== "8");
+
+const isChecked = true,
+      isClosed =  true;
+
+console.log(isChecked && isClosed);
+console.log(isChecked || isClosed);
+console.log(isChecked || !isClosed);
+
+
+
